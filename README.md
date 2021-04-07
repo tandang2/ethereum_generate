14321d9d2de64cd34c4b6bb2154c8d06dbecfbf0
pangea2015 <42212947+pangea2015@users.noreply.github.com>
2021-04-07 10:20:42 +0200

    readme

--- README.md
+++ README.md
@@ -1,1 +1,77 @@
-# ethereum_wallet_generator
+# Ethereum Wallet Generator
+> DISCLAIMER: Do NOT use this to do anything illegal (like stealing), and if you do so, please don't make me responsible. This is for educational purposes only!
+
+The Ethereum Wallet Generator is a little tool that can generate random ETH wallets and even check their balance. It achieves this, by generating a random private key, and calculating the corresponding public address. 
+> Fun fact: There are 2²⁵⁶ different possible combinations! (SHA-256)
+
+## Installation
+1. Make sure to install [Python](https://www.python.org/downloads/).
+2. Open your console and type: `python -m pip install eth-account`
+3. Download the code and extract the `.zip` file anywhere you want. 
+> You're done!
+
+## Usage
+Just open a new console in the same directory as the installed files.
+After typing `main.py --help`, you can see all commands you can use instead of `--help`.
+
+ The following help message will appear:
+```
+ethereum_wallet_generator>main.py --help
+usage: Ethereum Wallet Generator [-h] [-b] [-gen NUMBER | -geninf] [-q | -qb]
+
+A tool to generate valid Ethereum Wallets
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -b, --balance         If specified, checks the balance of each generated wallet. | SLOWS DOWN A LOT
+  -gen NUMBER, --generateWallets NUMBER
+                        generates a given number of wallets with private and public key
+  -geninf, --generateInfinite
+                        generates random wallets forever | -q recommended | CTRL + C to stop
+  -q, --quiet           prints only the output | -qq -qqq -qqqq for even less output
+  -qb, --quietbalance   only prints ETH Adresses with balance > 0 | propably won't find anything | useful for output
+                        to file
+```
+> Those little `[]` in the first line indicate so called `mutually exclusive groups`. Basically, you can only use one argument per group at the time. For example, you could either use `-q` or `-qb`, but not both at the same time.
+
+### Commands
+
+```
+Syntax: -gen NUMBER | --generateWallets NUMBER
+Usage: main.py -gen 4
+
+It will simply output the given number of random ETH wallets. 
+```
+```
+Syntax: -geninf | --generateInfinite
+Usage: main.py -geninf
+
+Never stops generating ranom wallets. To stop, use CTRL + C.
+```
+```
+Syntax: -b | --balance
+Usage: main.py -gen 1 -b
+
+Generates a random wallet and checks its balance using the etherscan.io API. 
+
+WARNING: This will drastically reduce generation speed, due to the limitation of Etherscan. Without using an API key it is limited to 1 per 5 seconds. With a valid API key in api_key.ini, it is limited to 5 per second.
+```
+```
+Syntax: -q / -qq / -qqq / -qqqq
+Usage: main.py -gen 1 -b -q
+
+-q: reduces output to: PRIVATEKEY ADDRESS BALANCE(if -b)
+-qq: reduces output to: PRIVATEKEY
+-qqq: reduces output to: ADDRESS
+-qqqq: reduces output to: BALANCE (even if -b is NOT given)
+```
+```
+Syntax: -qb | --quietbalance
+Usage: main.py -gen 1 -qb
+
+Special variation of: -q
+Only prints an output, if the balance is > 0.
+Due to the low chance of generating a wallet with actual money, it will propably never output anything. Trust me, it works!
+
+Pro tip: main.py -geninf -qb > out.txt
+```
--- main.py
+++ main.py
@@ -7,18 +7,18 @@ python -m pip install eth-account
 # import
 from eth_account import Account
 from os import urandom
-import urllib.request
+from urllib.request import urlopen
 from json import load
 from argparse import ArgumentParser
 from time import sleep
 
 # functions
-def generate_acc():
+def generate_acc(): # if called, generates a random ETH wallet
     global acc
     acc = Account.create(urandom(256))
     return acc
 
-def gen_output():
+def gen_output(): # generates output depending on the inputs
     generate_acc()
     if args.quiet == 1:
         if args.balance:
@@ -41,10 +41,9 @@ def gen_output():
                 print(f"Balance: {get_balance()} ETH")
 
 def get_balance():
-    global acc
-    with open("api_key.ini", "r") as key:
+    with open("api_key.ini", "r") as key: # checks if API key in "api_key.ini"
             if key.read() == "":
-                    call = urllib.request.urlopen(f"https://api.etherscan.io/api?module=account&action=balance&address={str(acc.address)}&tag=latest")
+                    call = urlopen(f"https://api.etherscan.io/api?module=account&action=balance&address={str(acc.address)}&tag=latest") # sends API request without key (longer time limits)
                     balance = load(call)["result"]
                     try:
                         return balance
@@ -52,43 +51,36 @@ def get_balance():
                         sleep(4.9)
             else:
                 for line in key.read():
-                    call = urllib.request.urlopen(f"https://api.etherscan.io/api?module=account&action=balance&address={str(acc.address)}&tag=latest&apikey={str(line)}")
+                    call = urlopen(f"https://api.etherscan.io/api?module=account&action=balance&address={str(acc.address)}&tag=latest&apikey={str(line)}") # sends API request with given key
                     balance = load(call)["result"]
                     try:
                         return balance
                     finally:
                         sleep(0.2)
-                    
 
-
-# argparse
-parser = ArgumentParser(description="A tool to generate valid Ethereum Wallets", prog="Ethereum Wallet Generator")
-parser.add_argument("-b", "--balance", help="If specified, checks the balance of each generated wallet. | SLOWS DOWN A LOT", action="store_true")
-
-gen_wallet = parser.add_mutually_exclusive_group()
-gen_wallet.add_argument("-gen", "--generateWallets", metavar="NUMBER", help="generates a given number of wallets with private and public key", type=int)
-gen_wallet.add_argument("-geninf", "--generateInfinite", help="generates random wallets forever | -q recommended | CTRL + C to stop", action="store_true")
-
-outprint = parser.add_mutually_exclusive_group()
-outprint.add_argument("-q", "--quiet", help="prints only the output | -qq -qqq -qqqq for even less output", action="count", default=0)
-outprint.add_argument("-qb", "--quietbalance", help="only prints ETH Adresses with balance > 0 | propably won't find anything | useful for output to file", action="store_true")
-
-args = parser.parse_args()
-
-# code
-if args.generateInfinite:
-    while True:
-        gen_output()
-
-if args.generateWallets != None and args.generateWallets > 0:
-    if args.quiet == 0:
-        for wallet in range(int(args.generateWallets)):
-            gen_output()
-    else:
-        for wallet in range(int(args.generateWallets)):
-            gen_output()
-
-# if opened directly
 if __name__ == "__main__":
+# argparse
+    parser = ArgumentParser(description="A tool to generate valid Ethereum Wallets", prog="Ethereum Wallet Generator")
+    parser.add_argument("-b", "--balance", help="If specified, checks the balance of each generated wallet. | SLOWS DOWN A LOT", action="store_true")
+
+    gen_wallet = parser.add_mutually_exclusive_group()
+    gen_wallet.add_argument("-gen", "--generateWallets", metavar="NUMBER", help="generates a given number of wallets with private and public key", type=int)
+    gen_wallet.add_argument("-geninf", "--generateInfinite", help="generates random wallets forever | -q recommended | CTRL + C to stop", action="store_true")
+
+    outprint = parser.add_mutually_exclusive_group()
+    outprint.add_argument("-q", "--quiet", help="prints only the output | -qq -qqq -qqqq for even less output", action="count", default=0)
+    outprint.add_argument("-qb", "--quietbalance", help="only prints ETH Adresses with balance > 0 | propably won't find anything | useful for output to file", action="store_true")
+    
+    args = parser.parse_args()
+    
+# code
+    if args.generateInfinite:
+        while True:
+            gen_output()
+
+    if args.generateWallets != None and args.generateWallets > 0:
+        for wallet in range(int(args.generateWallets)):
+            gen_output()
+
     if not any(vars(args).values()):
         gen_output()
